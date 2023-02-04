921e21fa59f862316f4310b56ba1833b54892800
Jaris Rener <jaris@leagueofgeeks.com>
2023-02-05 10:29:49 +1100

    Menu fix

--- GGJ2023_UnityProject/Assets/Scripts/StartMenu.cs
+++ GGJ2023_UnityProject/Assets/Scripts/StartMenu.cs
@@ -1,17 +1,15 @@
 namespace LemonBerry 
 {
-    using System.Collections;
-    using System.Collections.Generic;
     using UnityEngine;
 
     public class StartMenu : MonoBehaviour 
     {
-        public void PlayGame 
+        public void PlayGame()
         {
 
         }
 
-        public void QuitGame 
+        public void QuitGame()
         {
             Application.Quit();
             print("Quitting game. Goodbye!");
